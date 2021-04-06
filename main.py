42240d836f920327cd923bd03fc329659b4878ea
Ruben170 <35116541+ruben170@users.noreply.github.com>
2021-04-06 17:07:44 +0200

    Got contract communication working

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Api is running"}
+
+
+@app.post("/post/permissions/")
+async def set_Permission(sender: str, they: str):
+    return {"message": "Api is running"}
+
+@app.get("/get/permissions/")
+async def set_Permission(sender: str, they: str):
+    return {"message": "Api is running"}
