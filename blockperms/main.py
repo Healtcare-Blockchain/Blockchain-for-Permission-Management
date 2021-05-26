2d6b051d36a69eb5182a9ff885eaa64a46030082
Ruben170 <35116541+Ruben170@users.noreply.github.com>
2021-05-26 10:26:51 +0200

    readying for merch with docker fix branch

--- blockperms/main.py
+++ blockperms/main.py
@@ -4,14 +4,15 @@ from pydantic import BaseModel
 from functions import permission
 from functions import blockchain
 from functions import account
+import xml.etree.ElementTree as ET
 
 
 #to see api documentation go to your-link/docs or your-link/redoc
 
 class Permission(BaseModel):
-    sender: str
-    receiver: str
-    permission: Optional[bool] = None
+    SenderId: str
+    ReceiverId: str
+    Permission: Optional[bool] = None
     #type: # Optional[tuple] = None
 
 app = FastAPI()
@@ -20,17 +21,49 @@ app = FastAPI()
 async def root():
     return {"message": "Api is running"}
 
-@app.get("/permissions/check")
-async def check_permission():
-    return {"permission": permission.check_permission("0x490CD3cAbED9f706055e617Ed09F96a905E0BD31", "0x50b72d23E5F3c1E0002E2E4C44C2f01ddd605b6F")}
+@app.post("/permissions/check")
+async def check_permission(permission : Permission):
+    verry_secure_file = ET.parse('users.xml')
+    root = verry_secure_file.getroot()
+    # users = verry_secure_file.getElementByTagName('user')
+    sender = None
+    receiver = None
 
-@app.get("/permissions/set")
-async def set_permissions():
-    sender = '0x490CD3cAbED9f706055e617Ed09F96a905E0BD31'
-    sender_pass = 'root'
-    they = '0x50b72d23E5F3c1E0002E2E4C44C2f01ddd605b6F'
-    they_pass = 'root'
-    return {"Permission": permission.set_permission(sender, sender_pass, they, they_pass)}
+    for user in root:
+        print(user[1].attrib)
+        print(permission.SenderId)
+        if user[0].attrib == permission.SenderId:
+            sender = user[1].attrib
+        elif user[0].attrib == permission.SenderId:
+            receiver = user[1].attrib
+
+    if sender or receiver == None:
+        print(sender + receiver)
+        return {"Error": "One or more users dont exist"}
+
+    return {"permission": permission.check_permission(sender, receiver)}
+
+@app.post("/permissions/set")
+async def set_permissions(permission : Permission):
+    verry_secure_file = ET.parse('users.xml')
+    root = verry_secure_file.getroot()
+    # users = verry_secure_file.getElementByTagName('user')
+    sender = None
+    receiver = None
+
+    for user in root:
+        if user[0] == permission.SenderId:
+            sender = user[1]
+            sender_pass = user[2]
+        elif user[0] == permission.SenderId:
+            receiver = user[1]
+            receiver_pass = user[2]
+
+    if sender or receiver == None:
+        print(sender + receiver)
+        return {"Error": "One or more users dont exist"}
+
+    return {"Permission": permission.set_permission(sender, sender_pass, receiver, receiver_pass, permission.permission)}
 
 @app.get("/test")
 async def connection_check():
