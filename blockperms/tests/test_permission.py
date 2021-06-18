79546fd2a32bc11aa3285dfb3567b7f552e7d086
Patrick <patrick4561@outlook.com>
2021-06-18 15:59:43 +0200

    Unit test connection

--- blockperms/tests/test_permission.py
+++ blockperms/tests/test_permission.py
@@ -6,14 +6,15 @@ from web3 import Web3
 from functions.blockchain import connection_setup
 
 class BlockchainTest(unittest.TestCase):
+    @classmethod
     def setUpClass(self):
         # Set up initial connection
         self.connection = connection_setup()
 
     def test_connection(self):
         # Check if the connection is working
-        self.assertIsInstance(self.connection, Web3)
-        self.assertTrue(self.connection.isConnected())
+        self.assertTrue(self.connection.isConnected(), msg="Not connected to chain")
+        # self.assertEqual(type(self.connection), Web3, msg="Connection")
 
 if __name__ == '__main__':
     unittest.main()
